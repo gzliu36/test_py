abca0e6af234a5b904897b0baa9a595cc609a3fd
KZ <gzliu36@gmail.com>
2018-07-16 23:41:45 -0700

    gogo

--- test.py
+++ test.py
@@ -1,4 +1,19 @@
 print("HelloWorld")
-
 text="HelloWorld_Text!!"
 print(text)
+
+print("Round 2")
+a=5
+b=1
+c=0
+d-null
+h=null
+print("1")
+e=a/b
+print("2")
+f=a/c
+print("3")
+g=a/d
+print("4")
+i=a/h
+
